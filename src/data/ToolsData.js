16db9dbf30fd33d1c5e77727fbb2265a9e07c987
ahmed-razic <ahmed.razic@gmail.com>
2022-01-24 10:03:03 +0100

    initial commit

---
+++ src/data/ToolsData.js
@@ -0,0 +1,36 @@
+const toolsData = [
+  {
+    id: 1,
+    name: 'Handsaw',
+    model: 'SawDust 1000',
+    type: 'Manual',
+    available: true,
+    quantity: 3,
+  },
+  {
+    id: 2,
+    name: 'Drill',
+    model: 'Makita D150',
+    type: 'Electrical',
+    available: false,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: 'Router',
+    model: 'Bosch',
+    type: 'Electrical',
+    inUse: true,
+    quantity: 1,
+  },
+  {
+    id: 4,
+    name: 'Hammer',
+    model: 'ACME',
+    type: 'Manual',
+    inUse: false,
+    quantity: 2,
+  },
+];
+
+export default toolsData;
